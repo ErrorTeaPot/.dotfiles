621efce8d4f881c5bf4451688562ff5a9528de2e
glesaout <le.saout@riscure.com>
2024-07-17 09:59:35 +0200

    refractor alacritty themes

--- Nvim/.config/nvim/init.lua
+++ Nvim/.config/nvim/init.lua
@@ -569,7 +569,6 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         -- rust_analyzer = {},
-        -- ccls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,17 @@ fish_config theme choose "Dracula Official"
 if status is-interactive
 	starship init fish | source
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/errorteapot/Computer_stuff/tools/miniconda/bin/conda
+    eval /home/errorteapot/Computer_stuff/tools/miniconda/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/errorteapot/Computer_stuff/tools/miniconda/etc/fish/conf.d/conda.fish"
+        . "/home/errorteapot/Computer_stuff/tools/miniconda/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/errorteapot/Computer_stuff/tools/miniconda/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
