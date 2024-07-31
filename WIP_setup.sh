ca738fbda1c0366aa33996102fc6be498f640426
glesaout <gabin.lesaout@etudiant.univ-rennes.fr>
2024-07-31 16:25:02 +0200

    Add case-insensitive comparison

--- WIP_setup.sh
+++ WIP_setup.sh
@@ -9,7 +9,13 @@ sudo dnf install $packages
 # Check for directories and run stow if they exist
 for package in $packages
 do
+   # Enable case-insensitive pattern matching
+   shopt -s nocaseglob
+
    if [ -d "$package" ]; then
        stow "$package"
    fi
+
+   # Disable case-insensitive pattern matching
+   shopt -u nocaseglob
 done
