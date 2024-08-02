6ac022fd1bb7823de71e18153981ed9345a5cd98
ErrorTeaPot <98967114+ErrorTeaPot@users.noreply.github.com>
2024-08-02 16:41:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of packages to install
-packages="libsecret gnome-keyring fish hyprland hyprlock neovim stow virt-manager alacritty lightdm lightdm-gtk wofi"
+packages="libsecret gnome-keyring fish hyprland hyprlock neovim stow virt-manager alacritty lightdm lightdm-gtk wofi grim"
 
 # Install the packages
 sudo dnf install -y $packages
