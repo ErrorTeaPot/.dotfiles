0912178067de0ccbac9fdac942d5b395e6aa6cfe
ErrorTeaPot <98967114+ErrorTeaPot@users.noreply.github.com>
2024-08-03 20:12:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # List of packages to install
-packages="libsecret gnome-keyring fish hyprland hyprlock neovim stow virt-manager alacritty lightdm lightdm-gtk wofi grim"
+packages="libsecret gnome-keyring fish hyprland hyprlock neovim stow virt-manager alacritty lightdm lightdm-gtk wofi grim network-manager-applet waybar google-noto-color-emoji-fonts dunst brave-browser"
+
+# Add Brave repo
+
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
 # Install the packages
 sudo dnf install -y $packages
@@ -19,6 +25,8 @@ do
    # Disable case-insensitive pattern matching
    shopt -u nocaseglob
 done
+
+stow -v 1 fonts fontconfig Wallpapers starship
 
 # Enable lightdm service
 sudo systemctl enable lightdm.service
