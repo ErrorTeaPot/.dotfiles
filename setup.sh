2c93f90560a3e45e81988ea5a7cab7a66213f8c1
ErrorTeaPot <gabin.lesaout@etudiant.univ-rennes.fr>
2024-12-17 09:57:54 +0100

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Variable to specify the type of installation
+INSTALL_TYPE=$1
+
 # List of packages to install
-packages="libsecret gnome-keyring fish hyprland hyprlock neovim stow virt-manager alacritty sddm wofi grim network-manager-applet waybar google-noto-color-emoji-fonts dunst brave-browser hyprland swww hyprpolkitagent"
+if [ "$INSTALL_TYPE" == "minimal" ]; then
+   packages="libsecret gnome-keyring fish hyprland hyprlock neovim stow virt-manager alacritty sddm wofi grim network-manager-applet waybar google-noto-color-emoji-fonts dunst brave-browser hyprland swww hyprpolkitagent"
+elif [ "$INSTALL_TYPE" == "workstation" ]; then
+   packages="fish neovim stow virt-manager alacritty brave-browser"
+else
+    echo "Usage: $0 [minimal|workstation]"
+    exit 1
+fi
 
 # Create the default folders
 echo "Creating the default folders"
@@ -13,9 +23,11 @@ sudo dnf install -y dnf-plugins-core
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
-# Enable solopasha/hyprland copr repo
-echo "Adding the solopasha/hyprland copr repo"
-sudo dnf copr enable -y solopasha/hyprland
+if [ "$INSTALL_TYPE" == "minimal" ]; then
+   Enable solopasha/hyprland copr repo
+   echo "Adding the solopasha/hyprland copr repo"
+   sudo dnf copr enable -y solopasha/hyprland
+fi
 
 # Install the packages
 echo "Installing packages"
@@ -29,7 +41,7 @@ do
    shopt -s nocaseglob
 
    if [ -d "$package" ]; then
-       stow -v 1 "$package"
+      stow -v 1 "$package"
    fi
 
    # Disable case-insensitive pattern matching
@@ -38,9 +50,11 @@ done
 
 stow -v 1 fonts fontconfig Wallpapers starship
 
-# Enable lightdm service
-echo "Enabling SDDM"
-sudo systemctl enable sddm.service
+if [ "$INSTALL_TYPE" == "minimal" ]; then
+   Enable lightdm service
+   echo "Enabling SDDM"
+   sudo systemctl enable sddm.service
 
-# Change the target for the graphical one
-sudo systemctl set-default graphical.target
+   Change the target for the graphical one
+   sudo systemctl set-default graphical.target
+fi
