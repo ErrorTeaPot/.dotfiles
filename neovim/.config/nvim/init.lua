d9b4a6b081ef4d8556bd23672ead1a1d8a8c78d3
ErrorTeaPot <gabin.lesaout@etudiant.univ-rennes.fr>
2025-02-06 12:31:05 +0100

    Add duck.lua plugin

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -605,6 +605,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         rust_analyzer = {},
+        solc = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
