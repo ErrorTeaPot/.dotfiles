5e284b45df2f28dc72199929feee878d5d8da270
ErrorTeaPot <gabin.lesaout@etudiant.univ-rennes.fr>
2025-02-08 11:23:44 +0100

    Remove fish unused functions and add nvim alias

--- fish/.config/fish/functions/asm32.fish
+++
@@ -1,4 +0,0 @@
-function asm32 --wraps=/usr/include/asm/unistd_32.h --wraps='cat /usr/include/asm/unistd_32.h' --description 'alias asm32=cat /usr/include/asm/unistd_32.h'
-  cat /usr/include/asm/unistd_32.h $argv
-        
-end
--- fish/.config/fish/functions/asm64.fish
+++
@@ -1,4 +0,0 @@
-function asm64 --wraps='cat /usr/include/asm/unistd_64.h' --description 'alias asm64=cat /usr/include/asm/unistd_64.h'
-  cat /usr/include/asm/unistd_64.h $argv
-        
-end
