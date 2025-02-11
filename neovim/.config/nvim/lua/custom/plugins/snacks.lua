0f63b5368d9e20cb41342b24afa8aad848a27abd
ErrorTeaPot <gabin.lesaout@etudiant.univ-rennes.fr>
2025-02-11 11:06:03 +0100

    Add lazygit binds

--- neovim/.config/nvim/lua/custom/plugins/snacks.lua
+++ neovim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -104,6 +104,27 @@ return {
       desc = 'Git Blame Line',
     },
     {
+      '<leader>gf',
+      function()
+        Snacks.lazygit.log_file()
+      end,
+      desc = 'Lazygit Current File History',
+    },
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
+    {
+      '<leader>gl',
+      function()
+        Snacks.lazygit.log()
+      end,
+      desc = 'Lazygit Log (cwd)',
+    },
+    {
       '<leader>un',
       function()
         Snacks.notifier.hide()
