facd3cef79267e749127b9466b04a64164e6ca6e
ErrorTeaPot <github.z5tea@passinbox.com>
2025-04-23 11:38:26 +0200

    Update neovim config

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -158,43 +158,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
   -- [[ Basic Keymaps ]]
   --  See `:help vim.keymap.set()`
 
-  -- Set highlight on search, but clear on pressing <Esc> in normal mode
+  -- Set highlight on search
   vim.opt.hlsearch = true
-  vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
-  -- Diagnostic keymaps
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
-  -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
-  -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
-  -- is not what someone will guess without a bit more experience.
-  --
-  -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
-  -- or just use <C-\><C-n> to exit terminal mode
-  vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
-  -- TIP: Disable arrow keys in normal mode
-  -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-  -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-  -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-  -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
-  -- Keybinds to make split navigation easier.
-  --  Use CTRL+<hjkl> to switch between windows
-  --
-  --  See `:help wincmd` for a list of all window commands
-  vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-  vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-  vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-  vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-  -- [[ Basic Autocommands ]]
-  --  See `:help lua-guide-autocommands`
-
-  require 'kickstart.autocmds'
 
   -- [[ Install `lazy.nvim` plugin manager ]]
   --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -392,42 +357,6 @@ P.S. You can delete this when you're done too. It's your config now! :)
         -- Enable Telescope extensions if they are installed
         pcall(require('telescope').load_extension, 'fzf')
         pcall(require('telescope').load_extension, 'ui-select')
-
-        -- See `:help telescope.builtin`
-        local builtin = require 'telescope.builtin'
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>sS', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-        -- Slightly advanced example of overriding default behavior and theme
-        vim.keymap.set('n', '<leader>/', function()
-          -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-          builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-          })
-        end, { desc = '[/] Fuzzily search in current buffer' })
-
-        -- It's also possible to pass additional configuration options.
-        --  See `:help telescope.builtin.live_grep()` for information about particular keys
-        vim.keymap.set('n', '<leader>s/', function()
-          builtin.live_grep {
-            grep_open_files = true,
-            prompt_title = 'Live Grep in Open Files',
-          }
-        end, { desc = '[S]earch [/] in Open Files' })
-
-        -- Shortcut for searching your Neovim configuration files
-        vim.keymap.set('n', '<leader>sn', function()
-          builtin.find_files { cwd = vim.fn.stdpath 'config' }
-        end, { desc = '[S]earch [N]eovim files' })
       end,
     },
 
@@ -591,7 +520,16 @@ P.S. You can delete this when you're done too. It's your config now! :)
           clangd = {},
           -- gopls = {},
           pyright = {},
-          rust_analyzer = {},
+          rust_analyzer = {
+            settings = {
+              ['rust-analyzer'] = {
+                check = {
+                  command = 'clippy',
+                },
+                checkOnSave = true,
+              },
+            },
+          },
           solc = {}, -- solitidy
           tinymist = {}, -- Typst
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -943,7 +881,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
       },
     },
   })
-
+  require 'kickstart.autocmds'
+  require 'kickstart.keymaps'
   -- The line beneath this is called `modeline`. See `:help modeline`
   -- vim: ts=2 sts=2 sw=2 et
 end
---
+++ neovim/.config/nvim/lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,80 @@
+return {
+  'olimorris/codecompanion.nvim',
+  config = function()
+    require('codecompanion').setup {
+      prompt_library = {
+        ['Code Expert'] = {
+          strategy = 'chat',
+          description = 'Get some special advice from an LLM',
+          opts = {
+            short_name = 'expert',
+            auto_submit = true,
+            stop_context_insertion = true,
+            user_prompt = true,
+          },
+          prompts = {
+            {
+              role = 'system',
+              content = function(context)
+                return 'I want you to act as a senior '
+                  .. context.filetype
+                  .. ' developer. I will ask you specific questions and I want you to return concise explanations and codeblock examples.'
+              end,
+            },
+            {
+              role = 'user',
+              content = function(context)
+                local text = require('codecompanion.helpers.actions').get_code(context.start_line, context.end_line)
+
+                return 'I have the following code:\n\n```' .. context.filetype .. '\n' .. text .. '\n```\n\n'
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
+        },
+      },
+      adapters = {
+        mistral = function()
+          return require('codecompanion.adapters').extend('mistral', {
+            env = {
+              api_key = 'Rm8LVsBPtkS4XCqq4kuwrCWfkX4OnKfA',
+              url = 'https://codestral.mistral.ai',
+            },
+            schema = {
+              model = {
+                default = 'codestral-latest',
+              },
+              temperature = {
+                default = 0.7,
+              },
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = 'mistral',
+        },
+        inline = {
+          adapter = 'mistral',
+          keymaps = {
+            accept_change = {
+              modes = { n = '<leader>ca' },
+              description = '[c]ode - [a]ccept AI change',
+            },
+            reject_change = {
+              modes = { n = '<leader>cr' },
+              description = '[c]ode - [r]eject AI change',
+            },
+          },
+        },
+      },
+    }
+  end,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+}
