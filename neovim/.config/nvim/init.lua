344113b8f81c05a9a831de2db936bf1ccc6a5094
ErrorTeaPot <github.z5tea@passinbox.com>
2025-05-12 13:31:58 +0200

    Add nix lsp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -532,6 +532,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
           },
           solc = {}, -- solitidy
           tinymist = {}, -- Typst
+          nil_ls = {},
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
           --
           -- Some languages (like typescript) have entire language plugins that can be useful:
