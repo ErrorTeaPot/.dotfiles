8d9c4753acbfba34556f7c626f4612673d1611d1
ErrorTeaPot <github.z5tea@passinbox.com>
2025-06-19 07:21:15 +0200

    Update fish config to support nix

---
+++ fish/.config/fish/conf.d/nix.fish
@@ -0,0 +1,2 @@
+
+if test -e /home/errorteapot/.nix-profile/etc/profile.d/nix.fish; . /home/errorteapot/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,14 @@
 fish_config theme choose "Rosé Pine Moon"
 
 if status is-interactive
-	starship init fish | source
+    starship init fish | source
 end
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+
+# Use fish_add_path to prepend paths to PATH
+fish_add_path --prepend $BUN_INSTALL/bin
 
 set -e CARGO_HOME
 set -Ux CARGO_HOME $HOME/.config/cargo
@@ -23,5 +25,6 @@ set -Ux SSH_AUTH_SOCK /run/user/1000/gcr/ssh
 set -e XDG_CONFIG_HOME
 set -Ux XDG_CONFIG_HOME $HOME/.config
 
-set -e fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $HOME/.config/cargo/bin $fish_user_paths
+# Use fish_add_path to prepend paths to fish_user_paths
+fish_add_path --prepend $HOME/.local/bin
+fish_add_path --prepend $HOME/.config/cargo/bin
