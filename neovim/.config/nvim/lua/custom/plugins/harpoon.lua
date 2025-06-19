10869352e05e5895c18386ef5aac4e044b12f412
ErrorTeaPot <github.z5tea@passinbox.com>
2025-06-19 20:14:00 +0200

    Move back keymaps into plugin file

--- neovim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ neovim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -6,6 +6,34 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup {}
 
+    vim.keymap.set('n', '<leader>aa', function()
+      harpoon:list():add()
+    end, { desc = 'Add a file' })
+    --vim.keymap.set('n', '<C-e>', function()
+    --harpoon.ui:toggle_quick_menu(harpoon:list())
+    --end, { desc = 'Toggle harpoon quick menu' })
+
+    vim.keymap.set('n', '<C-h>', function()
+      harpoon:list():select(1)
+    end, { desc = 'Select 1st harpooned file' })
+    vim.keymap.set('n', '<C-t>', function()
+      harpoon:list():select(2)
+    end, { desc = 'Select 2nd harpooned file' })
+    vim.keymap.set('n', '<C-n>', function()
+      harpoon:list():select(3)
+    end, { desc = 'Select 3rd harpooned file' })
+    vim.keymap.set('n', '<C-s>', function()
+      harpoon:list():select(4)
+    end, { desc = 'Select 4th harpooned file' })
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<C-S-P>', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<C-S-N>', function()
+      harpoon:list():next()
+    end)
+
     -- basic telescope configuration
     local conf = require('telescope.config').values
     local function toggle_telescope(harpoon_files)
