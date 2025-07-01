0f854f9dc7985fb2f61e0c0afdbf01bf0299f080
ErrorTeaPot <github.z5tea@passinbox.com>
2025-07-01 08:52:24 +0200

    Remove useless lsps

--- neovim/.config/nvim/lua/config/plugins/lsp.lua
+++ neovim/.config/nvim/lua/config/plugins/lsp.lua
@@ -15,35 +15,9 @@ return { -- LSP Configuration & Plugins
     { 'folke/neodev.nvim', opts = {} },
   },
   config = function()
-    -- Brief aside: **What is LSP?**
-    --
-    -- LSP is an initialism you've probably heard, but might not understand what it is.
-    --
-    -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-    -- and language tooling communicate in a standardized fashion.
-    --
-    -- In general, you have a "server" which is some tool built to understand a particular
-    -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-    -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-    -- processes that communicate with some "client" - in this case, Neovim!
-    --
-    -- LSP provides Neovim with features like:
-    --  - Go to definition
-    --  - Find references
-    --  - Autocompletion
-    --  - Symbol Search
-    --  - and more!
-    --
-    -- Thus, Language Servers are external tools that must be installed separately from
-    -- Neovim. This is where `mason` and related plugins come into play.
-    --
     -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
     -- and elegantly composed help section, `:help lsp-vs-treesitter`
 
-    --  This function gets run when an LSP attaches to a particular buffer.
-    --    That is to say, every time a new file is opened that is associated with
-    --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-    --    function will be executed to configure the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -156,7 +130,6 @@ return { -- LSP Configuration & Plugins
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       clangd = {},
-      -- gopls = {},
       pyright = {},
       rust_analyzer = {
         settings = {
@@ -168,7 +141,6 @@ return { -- LSP Configuration & Plugins
           },
         },
       },
-      solc = {}, -- solitidy
       tinymist = {}, -- Typst
       nil_ls = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
--- neovim/.config/nvim/lua/config/plugins/mini.lua
+++ neovim/.config/nvim/lua/config/plugins/mini.lua
@@ -30,8 +30,5 @@ return { -- Collection of various small independent plugins/modules
     statusline.section_location = function()
       return '%2l:%-2v'
     end
-
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
   end,
 }
