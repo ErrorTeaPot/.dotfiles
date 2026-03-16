c3998f006f5cef4754ead5c6c7a5c39088e0715f
ErrorTeaPot <github.z5tea@passinbox.com>
2026-03-16 09:21:30 +0100

    neovim/neogen : change snippet engine

--- neovim/.config/nvim/lua/config/plugins/neogen.lua
+++ neovim/.config/nvim/lua/config/plugins/neogen.lua
@@ -3,9 +3,8 @@ return {
   config = function()
     -- Initialize Neogen
     require('neogen').setup {
-      snippet_engine = 'luasnip', -- or 'vsnip' or 'snippy' or 'none'
+      snippet_engine = 'nvim',
     }
-
     -- Set up key mapping
     vim.keymap.set('n', '<Leader>cd', require('neogen').generate, { desc = 'Generate code documentation' })
   end,
