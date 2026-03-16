c24c10a9f79b6d53acf86c74d5dcb001a7f3d4a9
ErrorTeaPot <github.z5tea@passinbox.com>
2026-03-16 09:15:21 +0100

    neovim : replace autocompletion by blink

---
+++ neovim/.config/nvim/lua/config/plugins/blink.lua
@@ -0,0 +1,18 @@
+return {
+  'saghen/blink.cmp',
+  version = '1.*',
+  opts = {
+    keymap = { preset = 'default' }, -- C-n/p, C-y, C-b/f, C-space
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+    completion = {
+      accept = { auto_brackets = { enabled = true } }, -- remplace l'intégration autopairs+cmp
+      documentation = { auto_show = true },
+    },
+    snippets = { preset = 'default' }, -- moteur de snippets intégré, LuaSnip non requis
+  },
+}
