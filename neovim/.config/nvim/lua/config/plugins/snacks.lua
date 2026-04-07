2b1bd46edf557ea250c1fae0104ebfa30dfd331a
ErrorTeaPot <github.z5tea@passinbox.com>
2026-04-07 16:31:04 +0200

    neovim/snacks : delete useless keymaps

--- neovim/.config/nvim/lua/config/plugins/snacks.lua
+++ neovim/.config/nvim/lua/config/plugins/snacks.lua
@@ -40,34 +40,6 @@ return {
   },
   keys = {
     {
-      '<leader>z',
-      function()
-        Snacks.zen()
-      end,
-      desc = 'Toggle Zen Mode',
-    },
-    {
-      '<leader>Z',
-      function()
-        Snacks.zen.zoom()
-      end,
-      desc = 'Toggle Zoom',
-    },
-    {
-      '<leader>.',
-      function()
-        Snacks.scratch()
-      end,
-      desc = 'Toggle Scratch Buffer',
-    },
-    {
-      '<leader>S',
-      function()
-        Snacks.scratch.select()
-      end,
-      desc = 'Select Scratch Buffer',
-    },
-    {
       '<leader>n',
       function()
         Snacks.notifier.show_history()
@@ -87,21 +59,6 @@ return {
         Snacks.rename.rename_file()
       end,
       desc = 'Rename File',
-    },
-    {
-      '<leader>gB',
-      function()
-        Snacks.gitbrowse()
-      end,
-      desc = 'Git Browse',
-      mode = { 'n', 'v' },
-    },
-    {
-      '<leader>gb',
-      function()
-        Snacks.git.blame_line()
-      end,
-      desc = 'Git Blame Line',
     },
     {
       '<leader>gf',
@@ -160,24 +117,6 @@ return {
       end,
       desc = 'Prev Reference',
       mode = { 'n', 't' },
-    },
-    {
-      '<leader>N',
-      desc = 'Neovim News',
-      function()
-        Snacks.win {
-          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = 'yes',
-            statuscolumn = ' ',
-            conceallevel = 3,
-          },
-        }
-      end,
     },
   },
   init = function()
