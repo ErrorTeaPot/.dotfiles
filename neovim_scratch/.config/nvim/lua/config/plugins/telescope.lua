cc447a9e5665c4e8519bc90350cd176b72b98b6f
ErrorTeaPot <glesaout@almond.eu>
2026-05-13 14:33:20 +0200

    neovim_scratch : add find keymaps

--- neovim_scratch/.config/nvim/lua/config/plugins/telescope.lua
+++ neovim_scratch/.config/nvim/lua/config/plugins/telescope.lua
@@ -6,3 +6,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, {desc = 'Telescope find keymaps' })
