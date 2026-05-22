589d92b5fb23b0db89c258c8ff2058e4157c7c26
ErrorTeaPot <glesaout@almond.eu>
2026-05-22 11:43:05 +0200

    neovim_scratch : new things

--- neovim_scratch/.config/nvim/init.lua
+++ neovim_scratch/.config/nvim/init.lua
@@ -18,10 +18,10 @@ vim.o.relativenumber = true
 -- Sync clipboard between OS and Neovim. Schedule the setting after `UIEnter` because it can
 -- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
 -- See `:h 'clipboard'`
-vim.api.nvim_create_autocmd('UIEnter', {
-  callback = function()
-    vim.o.clipboard = 'unnamedplus'
-  end,
+vim.api.nvim_create_autocmd("UIEnter", {
+	callback = function()
+		vim.o.clipboard = "unnamedplus"
+	end,
 })
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
@@ -32,53 +32,60 @@ vim.o.cursorline = true -- Highlight the line where the cursor is on.
 vim.o.scrolloff = 10 -- Keep this many screen lines above/below the cursor.
 vim.o.list = true -- Show <tab> and trailing spaces.
 
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+
 -- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
 vim.o.confirm = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 -- AUTOCOMMANDS (EVENT HANDLERS)
 -- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
 
 -- Highlight when yanking (copying) text.
 -- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function()
-    vim.hl.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 -- Restore cursor shape (solution simple et directe)
-vim.api.nvim_create_autocmd('VimLeave', {
-  desc = 'restore the cursor shape on exit of neovim',
-  callback = function()
-    vim.cmd('set guicursor=')
-    vim.fn.nvim_ui_send('\033[2 q')
-  end
+vim.api.nvim_create_autocmd("VimLeave", {
+	callback = function()
+		vim.opt.guicursor = ""
+	end,
 })
 
 -- USER COMMANDS: DEFINE CUSTOM COMMANDS
 -- See `:h nvim_create_user_command()` and `:h user-commands`
 
 -- Create a command `:GitBlameLine` that print the git blame for the current line
-vim.api.nvim_create_user_command('GitBlameLine', function()
-  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
-  local filename = vim.api.nvim_buf_get_name(0)
-  print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
-end, { desc = 'Print the git blame for the current line' })
+vim.api.nvim_create_user_command("GitBlameLine", function()
+	local line_number = vim.fn.line(".") -- Get the current line number. See `:h line()`
+	local filename = vim.api.nvim_buf_get_name(0)
+	print(vim.system({ "git", "blame", "-L", line_number .. ",+1", filename }):wait().stdout)
+end, { desc = "Print the git blame for the current line" })
 
 -- PLUGINS
 -- See `:h :packadd`, `:h vim.pack`
 
 -- Add the "nohlsearch" package to automatically disable search highlighting after
 -- 'updatetime' and when going to insert mode.
-vim.cmd('packadd! nohlsearch')
+vim.cmd("packadd! nohlsearch")
 
 -- Install third-party plugins via "vim.pack.add()".
 vim.pack.add({
-  -- Quickstart configs for LSP
-  'https://github.com/neovim/nvim-lspconfig',
+	-- Quickstart configs for LSP
+	"https://github.com/neovim/nvim-lspconfig",
 })
 
 -- Import plugins from lua/config/plugins
-require('config.plugins')
+require("config.plugins")
--- neovim_scratch/.config/nvim/lua/config/lsp.lua
+++ neovim_scratch/.config/nvim/lua/config/lsp.lua
@@ -1,47 +1,134 @@
--- Configuration LSP minimale pour Neovim 0.10+
--- Utilise uniquement l'API native de Neovim
+vim.pack.add({ "https://github.com/j-hui/fidget.nvim" })
+require("fidget").setup({})
 
--- Configuration basique des diagnostics
-vim.diagnostic.config({
-  virtual_text = true,  -- Affiche les erreurs dans le texte
-  signs = true,         -- Affiche les icônes dans la gouttière
-  update_in_insert = false,
+-- Enable the following language servers
+--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+--  See `:help lsp-config` for information about keys and how to configure
+---@type table<string, vim.lsp.Config>
+local servers = {
+	-- clangd = {},
+	-- gopls = {},
+	-- pyright = {},
+	-- rust_analyzer = {},
+	clangd = {
+		cmd = {
+			"clangd",
+			"--background-index",
+			"--clang-tidy",
+			"--completion-style=detailed",
+			"--header-insertion=never",
+		},
+		settings = {
+			["clangd"] = {
+				-- Activer l'auto-complétion détaillée
+				completion = {
+					detailedLabel = true,
+				},
+				-- Activer les diagnostics avancés
+				diagnostics = {
+					clangTidy = {
+						add = {
+							"modernize-*",
+							"performance-*",
+							"bugprone-*",
+							"readability-*",
+							"portability-*",
+							"clang-analyzer-*",
+						},
+					},
+				},
+			},
+		},
+	},
+	pyright = {},
+	rust_analyzer = {
+		settings = {
+			["rust-analyzer"] = {
+				check = {
+					command = "clippy",
+				},
+				checkOnSave = true,
+			},
+		},
+	},
+	marksman = {},
+	nil_ls = {},
+	--
+	-- Some languages (like typescript) have entire language plugins that can be useful:
+	--    https://github.com/pmizio/typescript-tools.nvim
+	--
+	-- But for many setups, the LSP (`ts_ls`) will work just fine
+	-- ts_ls = {},
+
+	stylua = {}, -- Used to format Lua code
+
+	-- Special Lua Config, as recommended by neovim help docs
+	lua_ls = {
+		on_init = function(client)
+			client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
+
+			if client.workspace_folders then
+				local path = client.workspace_folders[1].name
+				if
+					path ~= vim.fn.stdpath("config")
+					and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+				then
+					return
+				end
+			end
+
+			client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+				runtime = {
+					version = "LuaJIT",
+					path = { "lua/?.lua", "lua/?/init.lua" },
+				},
+				workspace = {
+					checkThirdParty = false,
+					-- NOTE: this is a lot slower and will cause issues when working on your own configuration.
+					--  See https://github.com/neovim/nvim-lspconfig/issues/3189
+					library = vim.tbl_extend("force", vim.api.nvim_get_runtime_file("", true), {
+						"${3rd}/luv/library",
+						"${3rd}/busted/library",
+					}),
+				},
+			})
+		end,
+		---@type lspconfig.settings.lua_ls
+		settings = {
+			Lua = {
+				format = { enable = false }, -- Disable formatting (formatting is done by stylua)
+			},
+		},
+	},
+}
+
+vim.pack.add({
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/mason-org/mason-lspconfig.nvim",
+	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
 })
 
--- Configuration de base pour un serveur LSP (exemple avec Lua)
--- Remplacez par le serveur que vous utilisez réellement
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'lua',
-  callback = function()
-    vim.lsp.start({
-      name = 'lua-language-server',
-      cmd = { 'lua-language-server' },
-      root_dir = vim.fs.dirname(vim.fs.find({ '.git', 'init.lua' }, { upward = true })[1]),
-      settings = {
-        Lua = {
-          runtime = { version = 'LuaJIT' },
-          diagnostics = { globals = { 'vim' } },
-          workspace = { library = vim.api.nvim_get_runtime_file('', true) },
-        },
-      },
-    })
-  end
+-- Automatically install LSPs and related tools to stdpath for Neovim
+require("mason").setup({})
+
+-- Ensure the servers and tools above are installed
+--
+-- To check the current status of installed tools and/or manually install
+-- other tools, you can run
+--    :Mason
+--
+-- You can press `g?` for help in this menu.
+local ensure_installed = vim.tbl_keys(servers or {})
+vim.list_extend(ensure_installed, {
+	-- You can add other tools here that you want Mason to install
 })
 
--- Mappings de base pour les fonctionnalités LSP
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local bufnr = args.buf
-    local opts = { buffer = bufnr }
-    
-    -- Définition
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts, { desc = 'Aller à la définition' })
-    
-    -- Documentation
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts, { desc = 'Documentation' })
-    
-    -- Navigation entre diagnostics
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts, { desc = 'Diagnostic précédent' })
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts, { desc = 'Diagnostic suivant' })
-  end
+require("mason-tool-installer").setup({
+	ensure_installed = ensure_installed,
 })
+
+for name, server in pairs(servers) do
+	vim.lsp.config(name, server)
+	vim.lsp.enable(name)
+end
