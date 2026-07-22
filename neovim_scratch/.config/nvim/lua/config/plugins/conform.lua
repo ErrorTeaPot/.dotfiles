c9c6df35317af9c13c11aa7401547435eb43d1cb
ErrorTeaPot <glesaout@almond.eu>
2026-07-22 11:19:08 +0200

    ajout de stylua comme formater

--- neovim_scratch/.config/nvim/lua/config/plugins/conform.lua
+++ neovim_scratch/.config/nvim/lua/config/plugins/conform.lua
@@ -24,6 +24,7 @@ require("conform").setup({
 		--
 		-- You can use 'stop_after_first' to run the first available formatter from the list
 		-- javascript = { "prettierd", "prettier", stop_after_first = true },
+		lua = { "stylua" },
 		markdown = { "markdownlint-cli2" },
 	},
 })
